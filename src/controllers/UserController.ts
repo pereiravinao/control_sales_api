78ca42a98b1545579dfcab0c6c890d7aa16553c4
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-08-07 19:05:40 -0300

    Cria controller para criação de usuário

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+import UserService from "../services/UserService";
+
+export class UserController {
+  async create(req: Request, res: Response) {
+    const data = await new UserService().create(req.body);    
+      return res.status(data.status).json({message: data.message})
+  }
+}
